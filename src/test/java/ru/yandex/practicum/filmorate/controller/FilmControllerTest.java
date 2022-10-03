18e50910b1dad23c523b41aa053ce49a3aa074ec
Oksana <ashkeeper@mail.ru>
2022-10-03 17:39:53 +0300

    implement: test postFilms

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
@@ -9,11 +11,31 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilmControllerTest {
+    FilmController filmController;
+    private final Film filmNullName = new Film(null, "Test description", LocalDate.of(1994, 5, 21), 100);
+    private final Film filmEmptyName = new Film("", "Test description", LocalDate.of(1994, 5, 21), 100);
+    private final Film filmEarlyRelease = new Film("TestName", "Test description", LocalDate.of(1895, 12, 27), 100);
+    private final Film filmNegativeDuration = new Film("TestName", "Test description", LocalDate.of(1994, 5, 21), -1);
+    private final Film film201description = new Film("TestName", "Test description Test description " +
+            "Test description Test description Test description Test description Test description " +
+            "Test description Test description Test description Test description Test descripti",
+            LocalDate.of(1994, 5, 21),
+            100
+    );
+    Film film = new Film("TestName",
+            "Test description Test description Test description Test description Test description " +
+                    "Test description Test description Test description Test description Test description " +
+                    "Test description Test descript",
+            LocalDate.of(1895, 12, 28),
+            0
+    );
+    @BeforeEach
+    public void init(){
+        filmController = new FilmController();
+    }
 
     @Test
     void getFilms() {
-        FilmController filmController = new FilmController();
-        Film film = new Film("TestName", "Test description", LocalDate.of(1994, 5, 21), 100);
         List<Film> filmsBefore = filmController.getFilms();
         filmController.postFilm(film);
         List<Film> filmsAfter = filmController.getFilms();
@@ -23,11 +45,31 @@ class FilmControllerTest {
         );
     }
 
-        @Test
-        void postFilm () {
-        }
+    @Test
+    void postFilm(){
+        List<Film> filmsBefore = filmController.getFilms();
 
-        @Test
-        void updateFilm () {
-        }
+        assertThrows(ValidationException.class,  () -> filmController.postFilm(filmNullName), "название не может быть пустым");
+        assertThrows(ValidationException.class,  () -> filmController.postFilm(filmEmptyName),"название не может быть пустым");
+        assertThrows(ValidationException.class,  () -> filmController.postFilm(film201description),"максимальная длина описания — 200 символов");
+        assertThrows(ValidationException.class,  () -> filmController.postFilm(filmEarlyRelease),"дата релиза — не раньше 28 декабря 1895");
+        assertThrows(ValidationException.class,  () -> filmController.postFilm(filmNegativeDuration),"продолжительность фильма должна быть положительной");
+
+        List<Film> filmsAfterIncorrectAddition = filmController.getFilms();
+
+        filmController.postFilm(film);
+
+        List<Film> filmsAfter = filmController.getFilms();
+        assertAll(
+                () -> assertEquals(0, filmsBefore.size(), "список пуст"),
+                () -> assertEquals(0, filmsAfterIncorrectAddition.size(), "список пуст"),
+                () -> assertEquals(1, filmsAfter.size(), "имеется оди элемент")
+        );
+
     }
+
+    @Test
+    void updateFilm() {
+
+    }
+}
