aef60b4f662257935646dc08ea7b322d36d3f667
Oksana <ashkeeper@mail.ru>
2022-10-03 22:59:33 +0300

    Revert "Delete: удалить валедации"
    This reverts commit 9174c9f012051180dad6cfb600408f091cfb4d3d.

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,12 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.*;
 
@@ -32,15 +30,16 @@ public class UserController {
     }
 
     @PostMapping
-    public User postUsers(@Valid @RequestBody User user) {
+    public User postUsers(@RequestBody User user) {
         try {
+            validation(user);
             user.setId(createId());
             if (user.getName() == null || user.getName().isBlank()) {
                 user.setName(user.getLogin());
             }
             users.put(user.getId(), user);
             log.info("Пользователь " + user.getLogin() + " добавлен");
-        } catch (RuntimeException e ) {
+        } catch (RuntimeException e) {
             log.warn(e.getMessage());
             throw new ValidationException(e);
         }
@@ -48,8 +47,9 @@ public class UserController {
     }
 
     @PutMapping
-    public User updateUsers(@Valid @RequestBody User user) {
+    public User updateUsers(@RequestBody User user) {
         try {
+            validation(user);
             if (users.containsKey(user.getId())) {
                 users.replace(user.getId(), user);
                 log.info("Пользователь " + user.getLogin() + " обновлен");
@@ -66,5 +66,14 @@ public class UserController {
     private List<User> getListUsers() {
         Collection<User> value = users.values();
         return new ArrayList<>(value);
+    }
+
+    private void validation(User user) throws ValidationException {
+        if (user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ValidationException("логин не может быть пустым и содержать пробелы");
+        }
+        if (user.getBirthday().isAfter(NOW_DATE)) {
+            throw new ValidationException("дата рождения не может быть в будущем");
+        }
     }
 }
