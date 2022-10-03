53673cda068fb037c9dc7e90deb742c58f6b08d6
Oksana <ashkeeper@mail.ru>
2022-10-03 14:31:38 +0300

    fix:заприватьть поля

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,17 +3,16 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 import lombok.NonNull;
 
-import java.time.Instant;
 import java.time.LocalDate;
 
 @Data
 public class User {
-    int id;
+    private int id;
     @NonNull
-    String email;
+    private String email;
     @NonNull
-    String login;
-    String name;
-    LocalDate birthday;
+    private String login;
+    private String name;
+    private LocalDate birthday;
 
 }
