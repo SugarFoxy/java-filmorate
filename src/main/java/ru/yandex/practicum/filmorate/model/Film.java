43ac90f075a89b08b42d52db4b14ddf79163a6eb
Oksana <ashkeeper@mail.ru>
2022-10-03 10:18:24 +0300

    feat: создать классы Film и User

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.model;
+
+
+import lombok.Data;
+
+
+import java.time.LocalDate;
+
+@Data
+public class Film {
+
+    private int id ;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private long duration;
+
+}
