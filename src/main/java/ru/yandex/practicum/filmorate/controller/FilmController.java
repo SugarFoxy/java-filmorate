4045f0aa70b13bc8c0c117913a63fbff4f6b7465
Oksana <ashkeeper@mail.ru>
2022-10-03 14:31:14 +0300

    fix:исправить ответы запросов у фильмов

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -4,29 +4,30 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
+@RequestMapping("/films")
 @Slf4j
 public class FilmController {
     private final Map<Integer, Film> films = new HashMap<>();
-    private static final LocalDate REFERENCE_POINT_RELEASE_DATE = LocalDate.of(1895,12,28);
-    private int id = 0;
+    private static final LocalDate REFERENCE_POINT_RELEASE_DATE = LocalDate.of(1895, 12, 28);
+    private int id = 1;
 
     public int createId() {
         return id++;
     }
 
-    @GetMapping("/films")
-    public Map<Integer, Film> getFilms() {
+    @GetMapping
+    public List<Film> getFilms() {
         log.info("Получен запрос на список фильмов");
-        return films;
+        return getListFilms();
     }
 
-    @PostMapping("/film")
+    @PostMapping
     public Film postFilm(@RequestBody Film film) {
         try {
             validation(film);
@@ -35,30 +36,42 @@ public class FilmController {
             log.info("Фильм добавлен");
         } catch (RuntimeException e) {
             log.warn(e.getMessage());
+            throw new RuntimeException(e);
         }
         return film;
     }
 
-    @PutMapping("/film")
+    @PutMapping
     public Film updateFilm(@RequestBody Film film) {
         try {
             validation(film);
-            films.replace(film.getId(), film);
-            log.info("Фильм онавлен");
+            if (films.containsKey(film.getId())) {
+                films.replace(film.getId(), film);
+                log.info("Фильм онавлен");
+            } else {
+                throw new ValidationException("Такого фильма не существует");
+            }
         } catch (RuntimeException e) {
             log.warn(e.getMessage());
+            throw new RuntimeException(e);
+
         }
         return film;
     }
 
+    private List<Film> getListFilms() {
+        Collection<Film> value = films.values();
+        return new ArrayList<>(value);
+    }
+
     private void validation(Film film) throws ValidationException {
-        if (film.getName().isEmpty() || film.getName().isBlank()) {
+        if (film.getName().isBlank()) {
             throw new ValidationException("название не может быть пустым");
         }
         if (film.getDescription().length() > 200) {
             throw new ValidationException("максимальная длина описания — 200 символов");
         }
-        if (film.getReleaseDate().isAfter(REFERENCE_POINT_RELEASE_DATE)) {
+        if (film.getReleaseDate().isBefore(REFERENCE_POINT_RELEASE_DATE)) {
             throw new ValidationException("дата релиза — не раньше 28 декабря 1895");
         }
         if (film.getDuration() < 0) {
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,17 +2,25 @@ package ru.yandex.practicum.filmorate.model;
 
 
 import lombok.Data;
-
+import lombok.NonNull;
 
 import java.time.LocalDate;
 
 @Data
+
 public class Film {
 
     private int id ;
+    @NonNull
     private String name;
     private String description;
     private LocalDate releaseDate;
     private long duration;
 
+    public Film(@NonNull String name, String description, LocalDate releaseDate, long duration) {
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+    }
 }
