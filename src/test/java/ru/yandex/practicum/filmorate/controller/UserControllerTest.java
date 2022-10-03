c5d41048bfd897728417725cefefb4b1b02790e2
Oksana <ashkeeper@mail.ru>
2022-10-03 17:53:10 +0300

    feat: создать класс для тестирования контролера пользователей

---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void getUsers() {
+    }
+
+    @Test
+    void postUsers() {
+    }
+
+    @Test
+    void updateUsers() {
+    }
+}
