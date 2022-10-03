8c4b169c759374f8674a3b1bf55e0c62eed293a1
Oksana <ashkeeper@mail.ru>
2022-10-03 16:02:47 +0300

    feat: создать класс для тестирования контролера фильмов

---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmControllerTest {
+
+    @Test
+    void getFilms() {
+    }
+
+    @Test
+    void postFilm() {
+    }
+
+    @Test
+    void updateFilm() {
+    }
+}
