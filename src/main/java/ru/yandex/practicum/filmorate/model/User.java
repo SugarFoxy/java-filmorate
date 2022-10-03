81807945334d93a53875f1a3488f3feb28208047
Oksana <ashkeeper@mail.ru>
2022-10-03 15:55:20 +0300

    update: добавить анатацию для email

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,16 +3,19 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 import lombok.NonNull;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @Data
 public class User {
     private int id;
     @NonNull
+    @NotBlank
+    @Email
     private String email;
     @NonNull
     private String login;
     private String name;
     private LocalDate birthday;
-
 }
