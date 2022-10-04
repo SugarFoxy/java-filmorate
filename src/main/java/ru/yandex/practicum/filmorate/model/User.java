52834d039f2845aca18083df0d290e8c60f302d9
Oksana <ashkeeper@mail.ru>
2022-10-04 20:55:39 +0300

    fix : добавить дополнительные анатации для проверки почты и логина

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,20 +1,23 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Data;
-import lombok.NonNull;
+import lombok.*;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class User {
     private int id;
-    @NonNull
-    @NotBlank
-    @Email
+    @NotNull(message = "Email не может быть null")
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Email должен быть действительной")
     private String email;
-    @NonNull
+    @NotNull(message = "логин не может быть null")
+    @NotBlank(message = "логин не может быть пустым")
     private String login;
     private String name;
     private LocalDate birthday;
