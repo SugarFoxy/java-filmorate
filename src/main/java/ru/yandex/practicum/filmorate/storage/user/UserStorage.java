d96257b92cf7afb057cb4b308559c8c720506ef6
Oksana <ashkeeper@mail.ru>
2022-10-24 18:47:16 +0300

    fix: исправить методы стореджа юзера

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -2,11 +2,13 @@ package ru.yandex.practicum.filmorate.storage.user;
 
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public interface UserStorage {
-    public List<User> getUsers();
-    public User postUsers(User user);
-    public User updateUsers(User user);
-    public User getUserById(Integer id );
+    List<User> getUsers();
+    User addUser(User user);
+    void updateUsers(User user);
+    User getUserById(Integer id );
 }
