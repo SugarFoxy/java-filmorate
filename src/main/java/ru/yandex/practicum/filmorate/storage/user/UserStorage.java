800da1bc7a17966ced5256b11b52dc05a0563bc7
Oksana <ashkeeper@mail.ru>
2022-10-24 10:37:13 +0300

    feat: добавить методы в интерфес пользователя

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,4 +1,11 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.List;
+
 public interface UserStorage {
+    public List<User> getUsers();
+    public User postUsers(User user);
+    public User updateUsers(User user);
 }
