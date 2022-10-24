5328e67383668ccf278ffdec29e4920a8d4c56ea
Oksana <ashkeeper@mail.ru>
2022-10-24 18:48:49 +0300

    feat:  добавить анатации для валидации у фильмов

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,18 +1,32 @@
 package ru.yandex.practicum.filmorate.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import org.springframework.lang.NonNull;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
-
+@Builder
+@AllArgsConstructor
 public class Film {
 
     private int id;
+    @NotBlank(message = "Название отсутствует")
     private String name;
+    @NotNull(message = "Филь не может быть без описания")
+    @Size(max = 200, message = "Максимальное коллычество смволов - 200")
     private String description;
+    @NotNull(message = "Дата не должна быть пустой")
     private LocalDate releaseDate;
+    @NotNull(message = "Продолжительность не может отсутствовать")
+    @Positive(message = "Продолжитьльность не может быть отрицательной")
     private long duration;
 
     public Film(String name, String description, LocalDate releaseDate, long duration) {
