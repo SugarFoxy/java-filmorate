1081ec2b84a40c674e0517a83456e79b60c3ed5d
Oksana <ashkeeper@mail.ru>
2022-10-24 10:45:38 +0300

    feat: логика получения списка друзей

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,4 +1,15 @@
 package ru.yandex.practicum.filmorate.service;
 
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.List;
+
+@Component
 public class UserService {
+
+    public List<User> findAllFriends(User user){
+        return user.getFriends();
+    }
+
 }
