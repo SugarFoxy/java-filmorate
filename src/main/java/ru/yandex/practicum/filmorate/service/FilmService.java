d6fbf60806f0e5dd46d0796c88408fbebb07bd1d
Oksana <ashkeeper@mail.ru>
2022-10-24 18:48:36 +0300

    feat:  добавить анатации для валидации у фильмов

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,12 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.model.Film;
-
-import java.time.LocalDate;
-import java.util.*;
 
 @Component
 public class FilmService {
