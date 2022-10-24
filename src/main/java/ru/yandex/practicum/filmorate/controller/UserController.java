42fc52e95ec4d972001cc081c7014055c1f326ab
Oksana <ashkeeper@mail.ru>
2022-10-24 18:47:48 +0300

    feat:  добавить обработчик ошибок

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,50 +1,82 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
-import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
+import javax.validation.ValidationException;
 import java.util.*;
 
 @RestController
 @RequestMapping("/users")
-
+@Validated
 @Slf4j
 public class UserController {
-    InMemoryUserStorage inMemoryUserStorage;
+    UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService =userService;
+    }
 
     @GetMapping
     public List<User> getUsers() {
         log.info("Получен запрос на список пользователей");
-        return inMemoryUserStorage.getUsers();
+        return userService.getUsers();
     }
 
     @PostMapping
-    public User postUsers(@Valid @RequestBody User user) {
-        try {
-            inMemoryUserStorage.postUsers(user);
-            log.info("Пользователь " + user.getLogin() + " добавлен");
-        } catch (RuntimeException e) {
-            log.warn(e.getMessage());
-            throw new ValidationException(e);
-        }
-        return user;
+    public User addUsers(@Valid @RequestBody User user) {
+        log.info("Получен запрос на создание пользавателя");
+        return userService.postUser(user);
     }
 
     @PutMapping
     public User updateUsers(@Valid @RequestBody User user) {
-        try {
-            inMemoryUserStorage.updateUsers(user);
-            log.info("Пользователь " + user.getLogin() + " обновлен");
-        } catch (RuntimeException e) {
-            log.warn(e.getMessage());
-            throw new ValidationException(e);
-        }
+        userService.updateUsers(user);
         return user;
+    }
+
+    @GetMapping("/{id}/friends")
+    public List<User> getUsersFriends(@PathVariable Integer id) {
+        return userService.findAllFriends(id);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public List<User> getMutualFriends(@PathVariable Integer id, @PathVariable Integer otherId) {
+        return userService.getMutualFriends(id, otherId);
+    }
+
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addToFriends(@PathVariable Integer id, @PathVariable Integer friendId) {
+        userService.addToFriends(id, friendId);
+    }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public void deleteFriends(@PathVariable Integer id, @PathVariable Integer friendId) {
+        userService.removeFromFriends(id, friendId);
+    }
+    @ExceptionHandler()
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String,String> handle(final MethodArgumentNotValidException e) {
+        String[] allErrors = e.getAllErrors().toString().split(";");
+        String massage = allErrors[allErrors.length-1];
+        Map<String,String> map = Map.of("error", massage);
+        log.warn(massage);
+        return map;
+    }
+
+    @ExceptionHandler()
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String,String> handle(final ValidationException e) {
+        Map<String,String> map = Map.of("error", e.getMessage());
+        log.warn(e.getMessage());
+        return map;
     }
 }
