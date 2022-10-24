a44351749dc757d4fce607849d780e935ae4be10
Oksana <ashkeeper@mail.ru>
2022-10-24 22:42:52 +0300

    fix: добавить метод

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -5,10 +5,12 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 public interface UserStorage {
     List<User> getUsers();
     User addUser(User user);
     void updateUsers(User user);
     User getUserById(Integer id );
+    public Map<Integer,User> getMapUsers();
 }
