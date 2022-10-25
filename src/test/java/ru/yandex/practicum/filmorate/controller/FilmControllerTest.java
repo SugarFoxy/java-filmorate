c9b1aed1feb16ecf87498a294b3ce703034d4036
Oksana <ashkeeper@mail.ru>
2022-10-25 23:12:11 +0300

    fix: заглушить тест контролера пользователя

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,18 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.junit.jupiter.api.Test;
-
 class FilmControllerTest {
 
-    @Test
-    void getFilms() {
-    }
-
-    @Test
-    void postFilm(){
-    }
-
-    @Test
-    void updateFilm() {
-    }
 }
