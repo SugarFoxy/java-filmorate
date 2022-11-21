d38facf9720bae726104a1095dc3992ae406968e
Oksana <ashkeeper@mail.ru>
2022-11-21 17:53:05 +0300

    fix: получить id  транзакции

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/impl/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/impl/UserDbStorage.java
@@ -5,21 +5,23 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.AbsenceOfObjectException;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.MPA;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.frends.FriendStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class UserDbStorage implements UserStorage {
     private final JdbcTemplate jdbcTemplate;
+    private final FriendStorage friendStorage;
 
-    public UserDbStorage(JdbcTemplate jdbcTemplate) {
+    public UserDbStorage(JdbcTemplate jdbcTemplate,
+                         @Qualifier("friendDbStorage") FriendStorage friendStorage) {
         this.jdbcTemplate = jdbcTemplate;
+        this.friendStorage = friendStorage;
     }
 
     @Override
@@ -30,12 +32,26 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public User addUser(User user) {
-        jdbcTemplate.update("INSERT INTO USERS (NAME, LOGIN,EMAIL,BIRTHDAY) " +
-                        "VALUES (?, ?, ?, ?);",
-                user.getName(),
-                user.getLogin(),
-                user.getEmail(),
-                user.getBirthday());
+        int id = 0;
+        try {
+            Connection connection = Objects.requireNonNull(jdbcTemplate.getDataSource()).getConnection();
+            String query = "INSERT INTO USERS (NAME, LOGIN,EMAIL,BIRTHDAY) VALUES (?, ?, ?, ?);";
+            try (PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+                statement.setString(1, user.getName());
+                statement.setString(2, user.getLogin());
+                statement.setString(3, user.getEmail());
+                statement.setDate(4, Date.valueOf(user.getBirthday()));
+                statement.executeUpdate();
+                try (ResultSet keys = statement.getGeneratedKeys()) {
+                    keys.next();
+                    id = keys.getInt(1);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        user.setId(id);
+        user.setFriends(friendStorage.getAllFriendByUser(id));
         return user;
     }
 
@@ -58,14 +74,19 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public User getUserById(Integer id) {
-        SqlRowSet filmRows = jdbcTemplate.queryForRowSet("select * from USERS where USER_ID = ?", id);
-        return User.builder()
-                .id(filmRows.getInt("user_id"))
-                .name(filmRows.getString("name"))
-                .login(filmRows.getString("login"))
-                .email(filmRows.getString("email"))
-                .birthday(filmRows.getDate("birthday").toLocalDate())
-                .build();
+        SqlRowSet userRows = jdbcTemplate.queryForRowSet("select * from USERS where USER_ID = ?", id);
+        if(userRows.next()) {
+            return User.builder()
+                    .id(userRows.getInt("user_id"))
+                    .name(userRows.getString("name"))
+                    .login(userRows.getString("login"))
+                    .email(userRows.getString("email"))
+                    .birthday(userRows.getDate("birthday").toLocalDate())
+                    .friends(friendStorage.getAllFriendByUser(userRows.getInt("user_id")))
+                    .build();
+        }else {
+            throw new AbsenceOfObjectException("такого пользователя нет");
+        }
     }
 
     private User makeUser(ResultSet rs) throws SQLException {
