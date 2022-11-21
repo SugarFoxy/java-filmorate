8c950ad1f8b454198917d755f65721a9325d2f76
Oksana <ashkeeper@mail.ru>
2022-11-21 18:57:21 +0300

    feat: добавить логирование запросов жанров

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -1,15 +1,16 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.genre.GenreStorage;
-import ru.yandex.practicum.filmorate.storage.genre.impl.GenreDbStorage;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class GenreService {
     private final GenreStorage storage;
 
@@ -19,10 +20,12 @@ public class GenreService {
     }
 
     public List<Genre> getAll() {
+        log.info("Получен запрос на список жанров");
         return storage.getAll();
     }
 
     public Genre getById(Integer id) {
+        log.info("Получен запрос на получение жанра по ID");
         return storage.getById(id);
     }
 }
