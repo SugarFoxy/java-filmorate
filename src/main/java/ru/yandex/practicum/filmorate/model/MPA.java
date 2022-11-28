51329c9b165b13e04879e3e60b1ec5ae220d3503
Oksana <ashkeeper@mail.ru>
2022-11-28 21:16:20 +0300

    delete: удалить реализацию через enum

--- src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
@@ -11,28 +11,6 @@ import ru.yandex.practicum.filmorate.exception.AbsenceOfObjectException;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MPA {
-
     private Integer id;
     private String name;
-
-    public MPA(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        switch (id) {
-            case 1:
-                return RatingMpa.G.getRating();
-            case 2:
-                return RatingMpa.PG.getRating();
-            case 3:
-                return RatingMpa.PG13.getRating();
-            case 4:
-                return RatingMpa.R.getRating();
-            case 5:
-                return RatingMpa.NC17.getRating();
-            default:
-                throw new AbsenceOfObjectException("Такого Мпа рейтинга не существует");
-        }
-    }
 }
