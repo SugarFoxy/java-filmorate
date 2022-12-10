e566467a5a6ab5e14e321681ffc5871dc8f76295
mic-uglov <mic-uglov@yandex.ru>
2022-12-10 17:38:35 +0300

    test: feed tests

---
+++ src/test/java/ru/yandex/practicum/filmorate/event/FeedEventPublishingTest.java
@@ -0,0 +1,60 @@
+package ru.yandex.practicum.filmorate.event;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.context.ApplicationEventPublisher;
+import ru.yandex.practicum.filmorate.dao.film.FilmStorage;
+import ru.yandex.practicum.filmorate.dao.friends.FriendsStorage;
+import ru.yandex.practicum.filmorate.dao.genre.GenreStorage;
+import ru.yandex.practicum.filmorate.dao.likes.LikesStorage;
+import ru.yandex.practicum.filmorate.dao.user.UserStorage;
+import ru.yandex.practicum.filmorate.model.FeedEvent;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.service.UserService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class FeedEventPublishingTest {
+    @Test
+    public void shouldPublishFeedEventWhenAddingAndDeletingFriend() {
+        FriendsStorage friendStorage = mock(FriendsStorage.class);
+        ApplicationEventPublisher publisher = mock(ApplicationEventPublisher.class);
+        UserService userService = new UserService(
+                mock(UserStorage.class), friendStorage, mock(FilmStorage.class), publisher);
+        ArgumentCaptor<FeedEvent> eventCaptor = ArgumentCaptor.forClass(FeedEvent.class);
+
+        when(friendStorage.addToFriends(1, 2)).thenReturn(100);
+        when(friendStorage.removeFromFriends(3, 4)).thenReturn(200);
+        doNothing().when(publisher).publishEvent(eventCaptor.capture());
+
+        userService.addFriend(1, 2);
+
+        assertEquals(100, eventCaptor.getValue().getEntityId());
+
+        userService.removeFriend(3, 4);
+
+        assertEquals(200, eventCaptor.getValue().getEntityId());
+    }
+
+    @Test
+    public void shouldPublishFeedEventWhenAddingOrDeletingLike() {
+        LikesStorage likesStorage = mock(LikesStorage.class);
+        ApplicationEventPublisher publisher = mock(ApplicationEventPublisher.class);
+        FilmService filmService = new FilmService(
+                mock(FilmStorage.class), likesStorage, mock(GenreStorage.class), publisher);
+        ArgumentCaptor<FeedEvent> eventCaptor = ArgumentCaptor.forClass(FeedEvent.class);
+
+        when(likesStorage.addLikeToFilm(1, 2)).thenReturn(100);
+        when(likesStorage.removeLikeFromFilm(3, 4)).thenReturn(200);
+        doNothing().when(publisher).publishEvent(eventCaptor.capture());
+
+        filmService.addLikeToFilm(1, 2);
+
+        assertEquals(100, eventCaptor.getValue().getEntityId());
+
+        filmService.removeLikeFromFilm(3, 4);
+
+        assertEquals(200, eventCaptor.getValue().getEntityId());
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/storages/EventStorageTest.java
@@ -0,0 +1,106 @@
+package ru.yandex.practicum.filmorate.storages;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import ru.yandex.practicum.filmorate.dao.event.EventStorage;
+import ru.yandex.practicum.filmorate.dao.user.UserStorage;
+import ru.yandex.practicum.filmorate.model.EventType;
+import ru.yandex.practicum.filmorate.model.FeedEvent;
+import ru.yandex.practicum.filmorate.model.Operation;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+public class EventStorageTest {
+    @Autowired
+    private EventStorage eventStorage;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private UserStorage userStorage;
+
+    @AfterEach
+    public void clear() {
+        jdbcTemplate.update("DELETE FROM users_model");
+    }
+
+    @Test
+    public void testGettingFeedWhenEmpty() {
+        assertEquals(Collections.emptyList(), eventStorage.getByUserId(1));
+    }
+
+    @Test
+    public void testSavingThenGetting() {
+        User user = new User("test@test.tst", "test", "test", LocalDate.now());
+
+        userStorage.addUser(user);
+        eventStorage.save(new FeedEvent(user.getId(), EventType.LIKE, Operation.ADD, 100));
+
+        List<FeedEvent> events = eventStorage.getByUserId(user.getId());
+
+        assertEquals(1, events.size());
+
+        FeedEvent event = events.get(0);
+
+        assertTrue(event.getTimestamp() > 0);
+        assertTrue(event.getEventId() > 0);
+        assertEquals(user.getId(), event.getUserId());
+        assertEquals(EventType.LIKE, event.getEventType());
+        assertEquals(Operation.ADD, event.getOperation());
+        assertEquals(100, event.getEntityId());
+    }
+
+    @Test
+    public void testTimestampDescendingOrder() {
+        User user = new User("test@test.tst", "test", "test", LocalDate.now());
+
+        userStorage.addUser(user);
+
+        FeedEvent event1 = new FeedEvent(user.getId(), EventType.FRIEND, Operation.REMOVE, 1);
+        FeedEvent event2 = new FeedEvent(user.getId(), EventType.LIKE, Operation.ADD, 2);
+
+        eventStorage.save(event2);
+        eventStorage.save(event1);
+
+        List<FeedEvent> events = eventStorage.getByUserId(user.getId());
+
+        assertEquals(2, events.size());
+        assertEquals(event2.getTimestamp(), events.get(0).getTimestamp());
+        assertEquals(event1.getTimestamp(), events.get(1).getTimestamp());
+    }
+
+    @Test
+    public void testGettingByDifferentUsers() {
+        User user1 = new User("test1@test.tst", "test1", "test1", LocalDate.now());
+        User user2 = new User("test2@test.tst", "test2", "test2", LocalDate.now());
+
+        userStorage.addUser(user1);
+        userStorage.addUser(user2);
+
+        FeedEvent event1 = new FeedEvent(user1.getId(), EventType.FRIEND, Operation.REMOVE, 1);
+        FeedEvent event2 = new FeedEvent(user2.getId(), EventType.LIKE, Operation.ADD, 2);
+
+        eventStorage.save(event1);
+        eventStorage.save(event2);
+
+        List<FeedEvent> events = eventStorage.getByUserId(user1.getId());
+
+        assertEquals(1, events.size());
+        assertEquals(1, events.get(0).getEntityId());
+
+        events = eventStorage.getByUserId(user2.getId());
+
+        assertEquals(1, events.size());
+        assertEquals(2, events.get(0).getEntityId());
+    }
+}
